78fe98d515a8dbece9a86ba679c18fe2fe1e8364
bdappelhans <bdappelhans@gmail.com>
2024-05-14 18:46:25 -0500

    Controller class content created, data members coded, run() function coded, menu option functions created but not coded yet

--- src/main/java/learn/house/ui/Controller.java
+++ src/main/java/learn/house/ui/Controller.java
@@ -1,4 +1,66 @@
 package learn.house.ui;
 
+import learn.house.data.DataException;
+import learn.house.domain.*;
+
 public class Controller {
+
+    private final GuestService guestService;
+    private final HostService hostService;
+    private final ReservationService reservationService;
+    private final View view;
+
+    public Controller(GuestService guestService, HostService hostService, ReservationService reservationService, View view) {
+        this.guestService = guestService;
+        this.hostService = hostService;
+        this.reservationService = reservationService;
+        this.view = view;
+    }
+
+    public void run() {
+        view.displayHeader("Welcome to Sustainable Foraging");
+        try {
+            runAppLoop();
+        } catch (DataException ex) {
+            view.displayException(ex);
+        }
+        view.displayHeader("Goodbye.");
+    }
+
+    private void runAppLoop() throws DataException {
+        MainMenuOption option;
+        do {
+            option = view.selectMainMenuOption();
+            switch (option) {
+                case VIEW_RESERVATIONS_BY_HOST:
+                    viewReservationsByHost();
+                    break;
+                case MAKE_RESERVATION:
+                    makeReservation();
+                    break;
+                case EDIT_RESERVATION:
+                    editReservation();
+                    break;
+                case CANCEL_RESERVATION:
+                    cancelReservation();
+                    break;
+            }
+        } while (option != MainMenuOption.EXIT);
+    }
+
+    private void viewReservationsByHost() {
+
+    }
+
+    private void makeReservation() {
+
+    }
+
+    private void editReservation() {
+
+    }
+
+    private void cancelReservation() {
+
+    }
 }
